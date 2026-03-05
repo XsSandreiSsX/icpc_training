d82dda565e58c19324b50ce7809ec6d69fb8a4a5
Andrey <s-and-8@mail.ru>
2026-03-05 08:24:17 +0500

    Training Day 15

--- sqrt_decomposition/problem_B.cpp
+++ sqrt_decomposition/problem_B.cpp
@@ -1,65 +1,90 @@
 #include <iostream>
 #include <cmath>
+#include <bits/stdc++.h>
+
+#define IOS ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 
 using namespace std;
 
 const int MAXN = 100005;
-
 long long arr[MAXN];
 int block_num[MAXN];
-bool start_block[MAXN];
-long long blck_sum[MAXN];
-
-long long blck_add[MAXN];
-long long lazy_add[MAXN];
+int block_start[MAXN];
+long long blocks[MAXN];
+long long add[MAXN];
 
 int main() {
+    //IOS;
     int n, q; cin >> n >> q;
 
     int len = sqrt(n);
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
         block_num[i] = i / len;
-        start_block[i] = (i % len == 0);
-        blck_sum[block_num[i]] += arr[i];
+        block_start[block_num[i]] = block_num[i] * len;
+        
+        if (block_start[block_num[i]] == i) {
+            blocks[block_num[i]] = arr[i];
+        } else {
+            blocks[block_num[i]] = max(blocks[block_num[i]], arr[i]);
+        }
     }
 
-    while(q--) {
+    while (q--) {
         int type; cin >> type;
         if (type == 1) {
             int l, r; cin >> l >> r;
             l--; r--;
 
-            long long ans = 0;
-
+            long long mmax = LLONG_MIN;
             int i = l;
             while (i <= r) {
-                if (start_block[i] && i + len - 1 <= r) {
-                    ans += (blck_sum[block_num[i]] + blck_add[block_num[i]]);
+                if (block_start[block_num[i]] == i && i + len - 1 <= r) {
+                    mmax = max(blocks[block_num[i]] + add[block_num[i]], mmax);
                     i += len;
+
                 } else {
-                    ans =+ (arr[i] + lazy_add[i]); 
+                    mmax = max(arr[i] + add[block_num[i]], mmax);
                     i += 1;
                 }
             }
 
-            cout << ans << "\n";
+            cout << mmax << "\n";
 
         } else {
             int l, r; cin >> l >> r;
             l--; r--;
-            long long x; cin >> x;
-            
+            long long val; cin >> val;
             int i = l;
-            while(i <= r) {
-                
-                if (start_block[i] && i + len - 1 <= r) {
-                    blck_sum[block_num[i]] += x;
+            while (i <= r) {
+                if (block_start[block_num[i]] == i && i + len - 1 <= r) {
+                    add[block_num[i]] += val;
                     i += len;
-                } else  i += 1;
-                lazy_add[i] += x;
+                } else {
+                    arr[i] += val;
+                    i += 1;
+                }
             }
-        }
+            
+            blocks[block_num[l]] = LLONG_MIN;
+            int cur_start = block_start[block_num[l]];
+            for (int i = cur_start; i < min(n, cur_start + len); i++) {
+                blocks[block_num[i]] = max(blocks[block_num[i]], arr[i]);
+
+                
+            }
+
+            blocks[block_num[r]] = LLONG_MIN;
+            cur_start = block_start[block_num[r]];
+            for (int i = cur_start; i < min(n, cur_start + len); i++) {
+                blocks[block_num[i]] = max(blocks[block_num[i]], arr[i]);
+
+                
+            }
+
+
+        } 
+
     }
 
 
--- test.py
+++
@@ -1,6 +0,0 @@
-tests = int(input())
-for _ in range(tests):
-    _ = int(input())
-    array = list(map(int, input().split()))
-    a = array.count(1)
-    
